fb08e589c87ce15f26becff9c5bb9aadc3e8a7e7
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-12-06 16:13:32 -0500

    Added new project dependances and updated styling files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider, Client, dedupExchange, fetchExchange } from 'urql'
+import { cacheExchange } from '@urql/exchange-graphcache'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const cache = cacheExchange({})
+
+const client = new Client({
+  url: 'http://localhost:4000',
+  exchanges: [dedupExchange, cache, fetchExchange],
+})
+
+ReactDOM.render(
+  <Provider value={client}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
