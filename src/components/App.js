53d37d263df555846e54f7073551e36812124c80
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-12-06 16:53:46 -0500

    Project routing complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
-// import LinkList from './LinkList'
+import { Switch, Route } from 'react-router-dom'
+
+import Header from './Header'
+import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 
-const App = () => <CreateLink />
+const App = () => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+      <Switch>
+        <Route exact path="/" component={LinkList} />
+        <Route exact path="/create" component={CreateLink} />
+      </Switch>
+    </div>
+  </div>
+)
 
 export default App
