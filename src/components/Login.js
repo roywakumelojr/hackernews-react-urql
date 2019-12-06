8dc41ef650b7cda28a867c954b4373bc26ceb707
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-12-06 17:35:06 -0500

    User Authentication Complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,23 @@
 import React from 'react'
+import gql from 'graphql-tag'
+import { useMutation } from 'urql'
 import { setToken } from '../token'
 
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`
 const Login = props => {
   // Used to switch between login and signup
   const [isLogin, setIsLogin] = React.useState(true)
@@ -9,6 +26,21 @@ const Login = props => {
   const [password, setPassword] = React.useState('')
   const [name, setName] = React.useState('')
   
+  const [state, executeMutation] = useMutation(
+    isLogin ? LOGIN_MUTATION : SIGNUP_MUTATION
+  );
+
+  const mutate = React.useCallback(() => {
+    executeMutation({ email, password, name })
+      .then(({ data }) => {
+        const token = data && data[isLogin ? 'login' : 'signup'].token
+        if (token) {
+          setToken(token)
+          props.history.push('/')
+        }
+      });
+  }, [executeMutation, props.history, isLogin, email, password, name]);
+
   return (
     <div>
       <h4 className="mv3">{isLogin ? 'Login' : 'Sign Up'}</h4>
@@ -40,12 +72,15 @@ const Login = props => {
         <button
           type="button"
           className="pointer mr2 button"
+          disabled={state.fetching}
+          onClick={mutate}
         >
           {isLogin ? "login" : "create account"}
         </button>
         <button
           type="button"
           className="pointer button"
+          disabled={state.fetching}
           onClick={() => setIsLogin(!isLogin)}
         >
           {isLogin ? 'need to create an account?' : 'already have an account?'}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
+import { getToken } from './token'
 
 import { Provider, Client, dedupExchange, fetchExchange } from 'urql'
 import { cacheExchange } from '@urql/exchange-graphcache'
@@ -11,7 +12,13 @@ const cache = cacheExchange({})
 
 const client = new Client({
   url: 'http://localhost:4000',
-  exchanges: [dedupExchange, cache, fetchExchange],
+  fetchOptions: () => {
+    const token = getToken()
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' }
+    }
+  },
+  exchanges: [dedupExchange, cache, fetchExchange]
 })
 
 ReactDOM.render(
