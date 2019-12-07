bee958ae3f79e9bd885dd345a469ec977f417da4
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-12-06 19:11:35 -0500

    Pagination and cache updates complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
 
 import Header from './Header'
 import LinkList from './LinkList'
@@ -11,12 +12,16 @@ const App = () => (
   <div className="center w85">
     <Header />
     <div className="ph3 pv1 background-gray">
-      <Switch>
-        <Route exact path="/" component={LinkList} />
-        <Route exact path="/create" component={CreateLink} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/search" component={Search} />
-      </Switch>
+      
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+          <Route exact path="/top" component={LinkList} />
+          <Route exact path="/new/:page" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
+        </Switch>
+      
     </div>
   </div>
 )
--- src/index.js
+++ src/index.js
@@ -8,7 +8,26 @@ import { getToken } from './token'
 import { Provider, Client, dedupExchange, fetchExchange } from 'urql'
 import { cacheExchange } from '@urql/exchange-graphcache'
 
-const cache = cacheExchange({})
+import { FEED_QUERY } from './components/LinkList'
+
+const cache = cacheExchange({
+  updates: {
+    Mutation: {
+      post: ({ post }, _args, cache) => {
+        const variables = { first: 10, skip: 0, orderBy: 'createdAt_DESC' }
+        cache.updateQuery({ query: FEED_QUERY, variables }, data => {
+          if (data !== null) {
+            data.feed.links.unshift(post)
+            data.feed.count++
+            return data
+          } else {
+            return null
+          }
+        })
+      }
+    }
+  }
+})
 
 const client = new Client({
   url: 'http://localhost:4000',
